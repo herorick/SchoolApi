ee5f5f3a7c1d5e6251e3bd15e43c477ade419783
Thanh Nguyen <nguyenhuuthanh@users.noreply.github.com>
2024-03-07 17:00:30 +0700

    update

--- src/utilities/PasswordUtility.ts
+++ src/utilities/PasswordUtility.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { Request } from "express";
 import { AuthPayload } from "interfaces/Auth";
 import { VendorPayload } from "interfaces/Vendor";
